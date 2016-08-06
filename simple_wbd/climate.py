c925ddbdd3c39d5633ba4465eadef1beb807d304
Miha Zidar <zidarsk8@gmail.com>
2016-08-06 17:32:44 +0200

    Add climate dataset helper functions

--- simple_wbd/climate.py
+++ simple_wbd/climate.py
@@ -23,12 +23,15 @@ class ClimateDataset(object):
     """
     # pylint: disable=too-few-public-methods
 
-    _time_key_map = {"decade": "year"}
 
-    KEYS = {"country", "type", "interval", "data"}
+    KEYS = ["country", "type", "interval", "data"]
+    _levels_map = {key: index for key, index in enumerate(KEYS)}
+    _time_key_map = {"decade": "year"}
 
     def __init__(self, api_responses):
         self.api_responses = api_responses
+        self.rows = None
+        self.columns = None
 
     def as_dict(self):
         """Return api responses as nested dictionary.
@@ -54,11 +57,54 @@ class ClimateDataset(object):
         for country, country_dict in self.api_responses.items():
             for type_, type_dict in country_dict.items():
                 for interval, interval_dict in type_dict.items():
-                    data = result[country][type_][interval]
+                    value_data = result[country][type_][interval]
                     for value in interval_dict.get("response", []):
                         time_key = self._time_key_map.get(interval, interval)
-                        data[value[time_key]] = value["data"]
+                        value_data[value[time_key]] = value["data"]
         return result
+
+    def _gather_key_by_level(self, data, level=0, accomulator=None):
+        """Gather all keys of a nested dict grouped by levels."""
+        if accomulator is None:
+            accomulator = defaultdict(set)
+
+        if not isinstance(data, dict):
+            return accomulator
+
+        for key, value in data.items():
+            accomulator[level].add(key)
+            self._gather_key_by_level(value, level+1, accomulator)
+
+        return accomulator
+
+    def _get_all_keys(self, data):
+        """Gather all key values by level for the current dataset."""
+        keys = self._gather_key_by_level(data)
+        countries = keys[0]
+        types = keys[1]
+        intervals = keys[2]
+        data_keys = keys[3]
+        return countries, types, intervals, data_keys
+
+    def _generate_list(self):
+        """Generate 2D array."""
+        dict_ = self.as_dict()
+        # countries, types, intervals, data_keys = self._get_all_keys(dict_)
+        return dict_
+
+    def as_list(self, columns=None):
+        """Get a 2D array data representation."""
+        if not columns:
+            self.columns = ["type", "interval", "data"]
+            self.rows = ["country"]
+        else:
+            self.columns = columns
+            self.rows = [key for key in self.KEYS if key not in columns]
+
+        if not set(self.columns) < set(self.KEYS):
+            raise TypeError("Columns argument contains invalid data")
+
+        return self._generate_list()
 
 
 class ClimateAPI(utils.ApiBase):
