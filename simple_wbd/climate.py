cf2ab9401ff9030dace9b5e41ba882ad7bc8863a
Miha Zidar <zidarsk8@gmail.com>
2016-08-07 02:45:34 +0200

    Add working as_list function

--- simple_wbd/climate.py
+++ simple_wbd/climate.py
@@ -4,9 +4,9 @@ This is a simple climate API helper focused on ease of use and getting data for
 use with Orange data mining software. For a more comprehensive APIs see wbpy or
 wbdata packages.
 """
-import itertools
 import json
 import logging
+from itertools import product
 
 from collections import defaultdict
 
@@ -23,8 +23,7 @@ class ClimateDataset(object):
     """
     # pylint: disable=too-few-public-methods
 
-
-    KEYS = ["country", "type", "interval", "data"]
+    KEYS = ["country", "type", "interval"]
     _levels_map = {key: index for key, index in enumerate(KEYS)}
     _time_key_map = {"decade": "year"}
 
@@ -63,7 +62,7 @@ class ClimateDataset(object):
                         value_data[value[time_key]] = value["data"]
         return result
 
-    def _gather_key_by_level(self, data, level=0, accomulator=None):
+    def _gather_keys_by_level(self, data, level=0, accomulator=None):
         """Gather all keys of a nested dict grouped by levels."""
         if accomulator is None:
             accomulator = defaultdict(set)
@@ -73,29 +72,90 @@ class ClimateDataset(object):
 
         for key, value in data.items():
             accomulator[level].add(key)
-            self._gather_key_by_level(value, level+1, accomulator)
+            self._gather_keys_by_level(value, level + 1, accomulator)
 
         return accomulator
 
+    def _gather_keys(self, data):
+        countries = set()
+        types = set()
+        intervals = set()
+        for country, c_dict in data.items():
+            countries.add(country)
+            for type_, t_dict in c_dict.items():
+                types.add(type_)
+                for interval, i_dict in t_dict.items():
+                    for key in i_dict:
+                        intervals.add((interval, key))
+
+        return countries, types, intervals
+
     def _get_all_keys(self, data):
         """Gather all key values by level for the current dataset."""
-        keys = self._gather_key_by_level(data)
-        countries = keys[0]
-        types = keys[1]
-        intervals = keys[2]
-        data_keys = keys[3]
-        return countries, types, intervals, data_keys
+        keys = self._gather_keys(data)
+        return {
+            "country": keys[0],
+            "type": keys[1],
+            "interval": keys[2],
+        }
+
+    @staticmethod
+    def _join(strings):
+        if isinstance(strings, (str, int)):
+            return str(strings)
+        return " - ".join(str(s) if isinstance(s, (str, int)) else self._join(s) for s in strings)
+
+    def _flaten(self, data):
+        return sum([[d] if isinstance(d, (str, int)) else self._flaten(d) for d in data], [])
+
+    def _get_level_key(self, row, column, key_name):
+        if key_name in self.rows:
+            if key_name == "interval":
+                return row[-2:]
+            return row[self.rows.index(key_name)]
+        elif key_name in self.columns:
+            if key_name == "interval":
+                return column[-2:]
+            return column[self.columns.index(key_name)]
+        return None
 
     def _generate_list(self):
         """Generate 2D array."""
         dict_ = self.as_dict()
-        # countries, types, intervals, data_keys = self._get_all_keys(dict_)
-        return dict_
+        all_keys = self._get_all_keys(dict_)
+        row_items = product(*[all_keys[row] for row in self.rows])
+        row_items = sorted([self._flaten(item) for item in row_items])
+        column_items = product(*[all_keys[c] for c in self.columns])
+        column_items = sorted([self._flaten(item) for item in column_items])
+
+        array = [[self.rows] + [col for col in column_items]]
+        column_count = len(column_items) + 1
+        row_count = len(row_items) + 1
+        for row in row_items:
+            array.append([row] + [None] * (column_count-1))
+
+        for row, column in product(range(1, row_count), range(1, column_count)):
+            interval, interval_key = self._get_level_key(array[row][0], array[0][column], "interval")
+            country = self._get_level_key(array[row][0], array[0][column], "country")
+            type_ = self._get_level_key(array[row][0], array[0][column], "type")
+            array[row][column] = dict_[country][type_][interval][interval_key]
+
+        # turn first column and row into strings
+        for i in range(len(array)):
+            array[i][0] = self._join(array[i][0])
+        for i in range(len(array[0])):
+            array[0][i] = self._join(array[0][i])
+
+        return array
 
     def as_list(self, columns=None):
-        """Get a 2D array data representation."""
+        """Get a 2D array data representation.
+
+        Note: if your data contains same decades and years the year collisions
+        will use only one value, either year 1990 or decade 1990.
+        """
         if not columns:
-            self.columns = ["type", "interval", "data"]
+            self.columns = ["type", "interval"]
             self.rows = ["country"]
         else:
             self.columns = columns
@@ -178,7 +238,7 @@ class ClimateAPI(utils.ApiBase):
             intervals = self._default_intervals
 
         api_responses = defaultdict(lambda: defaultdict(dict))
-        parameters = list(itertools.product(locations, data_types, intervals))
+        parameters = list(product(locations, data_types, intervals))
         self.progress["pages"] = len(parameters)
         self.progress["current_page"] = 0
         for location, data_type, interval in parameters:
